be68f2fa7172d5cde75ac6ffed283feb6c95f34e
Simon Bakan <simon.bakan@gmail.com>
2024-12-24 23:42:51 -0500

    Finished plus and times expressions

--- src/scheme/mod.rs
+++ src/scheme/mod.rs
@@ -1,15 +1,77 @@
 use std::fs::File;
 use std::io::{self, Read};
+use std::ops;
+
+use gcd::Gcd;
 
 #[derive(Debug)]
 enum Expr{
     Text(String),
-    SubExpr(Box<ParseTree>),
+    Tree(Box<ParseTree>),
 }
+use Expr::Text;
+use Expr::Tree;
 
 #[derive(Debug)]
 struct ParseTree{
     list: Vec<Expr>,
+}
+
+enum Val{
+    Number(bool, u32, u32),
+    Boolean(bool),
+    Function(ParseTree),
+    Name(String),
+    SchemeError(),
+}
+use Val::Number;
+use Val::Boolean;
+use Val::Function;
+use Val::Name;
+use Val::SchemeError;
+
+impl ops::Add<Val> for Val {
+    type Output = Val;
+
+    fn add(self, rhs: Val) -> Val {
+        if let Number(neg, n ,d) = self{
+            if let Number(oneg, on, od) = rhs{
+                let new_denom = d * od / d.gcd(od);
+                let mut n = n * new_denom / od;
+                let mut on = on * new_denom / d;
+                let mut neg = neg;
+if n > on {
+                    n = if neg{n - on} else {on + n};
+                }
+                if on > n {
+                    neg = oneg;
+                    n = if neg{on - n} else {on + n};
+                }
+
+                return Number(neg, n, new_denom);
+            }
+        }
+        SchemeError()
+    }
+}
+impl ops::Mul<Val> for Val {
+    type Output = Val;
+
+    fn mul(self, rhs: Val) -> Val {
+        if let Number(neg, n ,d) = self{
+            if let Number(oneg, on, od) = rhs{
+                let n = n * on;
+                let d = d * od;
+                let common = n.gcd(d);
+                let n = n / common;
+                let d = d / common;
+
+                let neg = neg != oneg;
+                return Number(neg, n, d);
+            }
+        }
+        SchemeError()
+    }
 }
 
 impl ParseTree{
@@ -43,7 +105,7 @@ where
             break;
         }
         if ch == "(" {
-            pt.add_expr(Expr::SubExpr(Box::new(build_tree(parsed))));
+            pt.add_expr(Tree(Box::new(build_tree(parsed))));
             m_ch = parsed.next();
             continue;
         }
@@ -51,6 +113,38 @@ where
         m_ch = parsed.next();
     }
     pt
+}
+
+fn apply_func(func: Val, val: Vec<Val>) -> Val{
+    match func{
+        Name(s) => {
+            match s.as_str(){
+                "+" => val.into_iter().fold(Number(false, 0, 1), |x, y| x + y),
+                "*" => val.into_iter().fold(Number(false, 1, 1), |x, y| x * y),
+                _ => SchemeError(),
+            }
+        }
+        _ => SchemeError(),
+    }
+}
+
+fn eval_scheme(ex: &Expr) -> Val{
+    match ex{
+        Text(txt) => if let Ok(n) = txt.parse(){ Number(false, n , 1) } else{Name(String::from(txt))},
+        Tree(expr) => {
+            let mut vals: Vec::<Val> = Vec::new();
+            let func = eval_scheme(&expr.list[0]);
+            let mut first = false;
+            for exp in &expr.list {
+                if !first {
+                    first = true;
+                    continue;
+                }
+                vals.push(eval_scheme(&exp))
+            }
+            apply_func(func, vals)
+        }
+    }
 }
 
 
@@ -62,7 +156,33 @@ pub fn run_scheme(s: &str)-> Result<String, io::Error> {
     let parsed = tokenize_scheme(&text);
 
     let tree = build_tree(&mut (parsed.into_iter()));
-    println!("{tree:?}");
+    //println!("{tree:?}");
+
+    for expr in tree.list{
+        let result = eval_scheme(&expr);
+        match result{
+            Number(neg, n, d) => {
+                if neg {
+                    print!("-")
+                }
+                print!("{n}");
+                if d != 1 {
+                    print!("/{d}");
+                }
+                println!("");
+                ()
+            },
+            Boolean(b) => {
+                println!("{b}");
+                ()
+            }
+            SchemeError() => {
+                println!("Error");
+                ()
+            }
+            _ => ()
+        }
+    }
 
 
     Ok(text)
